4ae4403b169a22f772ba6ba07db6b3b8f07a4d3f
王瀚宇 <huw12313212@gmail.com>
2014-09-24 19:32:52 +0800

    Controller for android

--- Assets/TouchManager.cs
+++ Assets/TouchManager.cs
@@ -16,49 +16,80 @@ public class TouchManager : MonoBehaviour {
 
 	public GameObject lastClicked = null;
 	public Vector3 lastPosition = Vector3.zero;
+	public Vector3 lastTouch = Vector3.zero;
 	public Camera nowCamera;
+
+	void MouseUpdate ()
+	{
+		Vector3 pos = nowCamera.ScreenToWorldPoint (Input.mousePosition);
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit2D hitInfo = Physics2D.Raycast (pos, Vector2.zero);
+			Debug.Log (pos);
+			if (hitInfo) {
+				Debug.Log ("hit");
+				if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer ("Eye")) {
+					lastClicked = hitInfo.transform.gameObject;
+				}
+			}
+		}
+		else
+			if (Input.GetMouseButtonUp (0)) {
+				lastClicked = null;
+			}
+			else
+				if (lastClicked != null) {
+					Vector3 offset = pos - lastPosition;
+					lastClicked.transform.position += offset;
+					Debug.Log ("pos:" + lastClicked);
+					SpriteRenderer sr = lastClicked.GetComponent<SpriteRenderer> ();
+					networkManager.UpdatePosition (lastClicked.name, lastClicked.transform.position.x * 100 + 320 / 2, -lastClicked.transform.position.y * 100 + 240 / 2);
+				}
+		lastPosition = pos;
+	}
+
+	void TouchUpdate ()
+	{
+
+		Touch t = Input.GetTouch (Input.touchCount-1);
+
+		Vector3 pos = nowCamera.ScreenToWorldPoint (t.position);
+
+		if(t.phase == TouchPhase.Began)
+		{
+			lastTouch = pos;
+			RaycastHit2D hitInfo = Physics2D.Raycast (pos, Vector2.zero);
+			Debug.Log (pos);
+			if (hitInfo) 
+			{
+				Debug.Log ("hit");
+				if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer ("Eye")) 
+				{
+					lastClicked = hitInfo.transform.gameObject;
+				}
+			}
+		}
+		else if (t.phase==TouchPhase.Ended || t.phase==TouchPhase.Canceled) 
+		{
+			lastClicked = null;
+		}
+		else if (lastClicked != null) 
+		{
+			Vector3 offset = pos - lastPosition;
+			lastClicked.transform.position += offset;
+			Debug.Log ("pos:" + lastClicked);
+			SpriteRenderer sr = lastClicked.GetComponent<SpriteRenderer> ();
+			networkManager.UpdatePosition (lastClicked.name, lastClicked.transform.position.x * 100 + 320 / 2, -lastClicked.transform.position.y * 100 + 240 / 2);
+			lastTouch = pos;
+		}
+		
+		lastTouch = pos;
+	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 pos = nowCamera.ScreenToWorldPoint (Input.mousePosition);
-
-		if (Input.GetMouseButtonDown (0)) {
-		
-						RaycastHit2D hitInfo = Physics2D.Raycast (pos, Vector2.zero);
-						
-			Debug.Log(pos);
-
-						if (hitInfo) {
-
-				Debug.Log("hit");
-								if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer ("Eye")) {
-										lastClicked = hitInfo.transform.gameObject;
-								}
-						}
-				}
-				else if (Input.GetMouseButtonUp (0)) 
-				{
-					lastClicked = null;
-				} 
-				else if (lastClicked != null) 
-				{
-					Vector3 offset = pos - lastPosition;
-					lastClicked.transform.position += offset;
-					Debug.Log("pos:"+lastClicked);
-					SpriteRenderer sr = lastClicked.GetComponent<SpriteRenderer>();
-
-			//Debug.Log(sr.bounds.size);
-					//float width = sr.bounds.size.x*100/2;
-					//float height = sr.bounds.size.y*100 /2;
-					
-			networkManager.UpdatePosition(lastClicked.name,
-			                              lastClicked.transform.position.x*100 + 320/2 ,
-			                              -lastClicked.transform.position.y*100 + 240/2);
-					
-				}
+		MouseUpdate ();
 
 
-		lastPosition = pos;
 	}
 }
