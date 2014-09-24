cab6ba5467cfa3c6469112465c129250c5de807f
王瀚宇 <huw12313212@gmail.com>
2014-09-24 17:18:09 +0800

    Eye Control

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+
+
+public class NetworkManager : MonoBehaviour {
+
+	public string WSAddress = "ws://10.0.1.37";
+	public string WSPort = "9300";
+	public delegate void ReceivedMessage(string str);
+
+	WebSocket ws;
+	// Use this for initialization
+	void Start () {
+
+		ws = new WebSocket (WSAddress+":"+WSPort);
+		ws.Connect ();
+		Debug.Log ("test");
+
+		ws.OnMessage += (object sender, MessageEventArgs e) => 
+		{
+			Debug.Log(e.Data);
+		};
+	}
+
+	public void SendMessage(string str)
+	{
+		ws.Send(str);
+	}
+
+	public void UpdatePosition(string name,float x,float y)
+	{
+			JSONObject data = new JSONObject();
+			
+			data.AddField("command","updatePosition");
+			data.AddField("target",name);
+			data.AddField("x",x);
+			data.AddField("y",y);
+			ws.Send(data.ToString());
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		/*
+		if (Input.GetKeyDown (KeyCode.W))
+		{
+			JSONObject data = new JSONObject();
+
+			data.AddField("command","updatePosition");
+			data.AddField("target","eye");
+			data.AddField("x",0);
+			data.AddField("y",0);
+
+			Debug.Log("W pressed");
+			ws.Send(data.ToString());
+		}*/
+	}
+}
---
+++ Assets/TouchManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+	public NetworkManager networkManager;
+
+
+	//public WebSocketTest
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	public GameObject lastClicked = null;
+	public Vector3 lastPosition = Vector3.zero;
+	public Camera nowCamera;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 pos = nowCamera.ScreenToWorldPoint (Input.mousePosition);
+
+		if (Input.GetMouseButtonDown (0)) {
+		
+						RaycastHit2D hitInfo = Physics2D.Raycast (pos, Vector2.zero);
+						
+			Debug.Log(pos);
+
+						if (hitInfo) {
+
+				Debug.Log("hit");
+								if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer ("Eye")) {
+										lastClicked = hitInfo.transform.gameObject;
+								}
+						}
+				}
+				else if (Input.GetMouseButtonUp (0)) 
+				{
+					lastClicked = null;
+				} 
+				else if (lastClicked != null) 
+				{
+					Vector3 offset = pos - lastPosition;
+					lastClicked.transform.position += offset;
+					Debug.Log("pos:"+lastClicked);
+					SpriteRenderer sr = lastClicked.GetComponent<SpriteRenderer>();
+
+			//Debug.Log(sr.bounds.size);
+					//float width = sr.bounds.size.x*100/2;
+					//float height = sr.bounds.size.y*100 /2;
+					
+			networkManager.UpdatePosition(lastClicked.name,
+			                              lastClicked.transform.position.x*100 + 320/2 ,
+			                              -lastClicked.transform.position.y*100 + 240/2);
+					
+				}
+
+
+		lastPosition = pos;
+	}
+}
